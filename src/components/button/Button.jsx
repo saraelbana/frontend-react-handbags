a8c1dc04f8fa754fc586607440f68eb55d367b81
sara <matika20@gmail.com>
2024-05-29 21:42:24 +0200

    simplifying the code by the button disable attribute

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,13 +1,13 @@
 import './Button.css';
 
 function Button({name, clickable}) {
-    let disableButton = true;
-    if(clickable === "true"){
-        disableButton = false;
-    }
-    else disableButton = true;
+    // let disableButton = true;
+    // if(clickable === "true"){
+    //     disableButton = false;
+    // }
+    // else disableButton = true;
     return(
-        <button className="button" disabled={disableButton} >
+        <button className="button" disabled={clickable !== "true"} >
             {name}
         </button>
     )
